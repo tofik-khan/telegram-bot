00d1c3cf3f4227cade7c4f198ec6ef2ff909f6af
Tofik Khan <tofikahmad@live.com>
2022-12-23 21:34:00 -0800

    Connect to Telegram API and send message via marco_bot

--- packages/telegram/marco/main.js
+++ packages/telegram/marco/main.js
@@ -1,6 +1,29 @@
-function main(args) {
-    let name = args.name || 'stranger'
-    let greeting = 'Hello ' + name + '!'
-    console.log(greeting)
-    return {"body": greeting}
+var axios = require('axios');
+
+exports.main = () => {
+
+    // Get Chat info
+    const chat_id = "@tofik_testbot";
+
+    // Create Message Body
+    const message = "I am now sending my first message from Digital Ocean Function";
+
+    // Create URL
+    const url = `https://api.telegram.org/bot${process.env.BOT_ID}/sendMessage?chat_id=${chat_id}&text=${message}?&parse_mode=markdown`
+
+    const config = {
+        method: 'post',
+        url: url,
+        headers: { }
+      };
+    
+    axios(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    return {"body": "Process Complete"};
 }
