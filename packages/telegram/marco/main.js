3cbea42a1603ae5153e81e13658680c5df868403
Tofik A. Khan <55766480+tofik-khan@users.noreply.github.com>
2022-12-24 17:06:59 -0800

    Connect to Telegram API and send message via marco_bot (#1)

--- packages/telegram/marco/main.js
+++ packages/telegram/marco/main.js
@@ -1,6 +1,31 @@
-function main(args) {
-    let name = args.name || 'stranger'
-    let greeting = 'Hello ' + name + '!'
-    console.log(greeting)
-    return {"body": greeting}
+var axios = require('axios');
+
+exports.main = () => {
+
+    // Get Chat info
+    const chat_id = "-1001815542928";
+
+    // Create Message Body
+    const message = `ٱلسَّلَامُ عَلَيْكُمْ \nThis is me testing a private chat ID`;
+
+    const encoded_message = encodeURIComponent(message);
+
+    // Create URL
+    const url = `https://api.telegram.org/bot${process.env.BOT_ID}/sendMessage?chat_id=${chat_id}&text=${encoded_message}?&parse_mode=markdown`
+
+    const config = {
+        method: 'post',
+        url: url,
+        headers: { }
+      };
+    
+      axios(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    return {"body": "Process Complete"};
 }
